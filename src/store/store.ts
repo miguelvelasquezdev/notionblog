eab210b9a22afaf159c91f34d76b09bdb354aa9b
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-02-19 16:14:37 -0500

    Fix store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,13 @@ import { create } from 'zustand'
 
 type State = {
   count: number
+  annotation: string
   increment: () => void
   clear: () => void
 }
 
 export const useStore = create<State>((set) => ({
+  count: 0,
   annotation: '',
   increment: () => set((state) => ({ count: state.count + 1 })),
   clear: () => set((state) => ({ count: 0 })),
